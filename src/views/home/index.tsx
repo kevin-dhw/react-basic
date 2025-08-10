f7296c3747be83b96cb6f9b7e8a1d98d89b64129
kevin <932731452@qq.com>
2025-08-10 17:38:00 +0800

    1

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,9 +1,17 @@
 import React from "react";
+import classNames from "classnames";
 
 const Home: React.FC = () => {
   return (
     <div className=" flex gap-4">
-      <div>content</div>
+      <div
+        className={classNames(
+          " h-[100px] w-[100px] border shadow-[1px_1px_10px_1px_#000]",
+          " transition-all ease-in-out delay-100 duration-1000 hover:scale-110 hover:bg-red-500"
+        )}
+      >
+        content
+      </div>
     </div>
   );
 };
